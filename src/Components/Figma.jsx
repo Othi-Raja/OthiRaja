4ba3a597f423657d241d957dcfb804651b252af1
Your Name <you@example.com>
2024-06-05 08:24:58 +0530

    "Updated Figma.css and Figma.jsx files, removed .card-img1 class, updated image source and URL in Figma.jsx, and added a new item to PersonalWork.json"

--- src/Components/Figma.jsx
+++ src/Components/Figma.jsx
@@ -9,7 +9,7 @@ const Figma = () => {
   useEffect(() => {
     let link = document.querySelector('.link')
     link.addEventListener('click', function () {
-      var url = "https://www.figma.com/proto/ULmOtappLI3cGhhjDEMXAQ/React-Portfolio?type=design&node-id=190-24&t=fLAp59l5IhkTWoxq-1&scaling=scale-down&page-id=0%3A1&mode=design"
+      var url = "https://othi-raja.github.io/Wowclothes_landing_Page/"
       window.open(url, '_blank')
     })
     //     var textWrapper = document.querySelector('.ml12');
@@ -47,20 +47,7 @@ const Figma = () => {
             {/* card--1 */}
             <div className="card link">
               <div className="card-img card-img1 "></div>
-              <h2 className="figma-title">
-                Portfolio
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="28"
-                  height="28"
-                  fill="currentColor"
-                  className="bi bi-link-45deg"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M4.715 6.542 3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1.002 1.002 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4.018 4.018 0 0 1-.128-1.287z" />
-                  <path d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 1 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 1 0-4.243-4.243z" />
-                </svg>
-              </h2>
+             <img src="https://firebasestorage.googleapis.com/v0/b/image-f2f77.appspot.com/o/Wowclothes_figma.jpg?alt=media&token=2ba41e27-d013-4534-9f4e-ec3c9cac1328" alt="" />
             </div>
           </Col>
           <Col md={4} data-aos="fade-up"
