e59bb785f4dbd68a2a3f20d0148b77b4125126b7
Your Name <you@example.com>
2024-06-07 17:00:23 +0530

    "Moved Firebase config values to environment variables and updated .gitignore to ignore .env file"

--- src/Components/FirebaseConfig.js
+++ src/Components/FirebaseConfig.js
@@ -3,14 +3,14 @@ import { initializeApp } from "firebase/app";
 // import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAZrXqVD7NVdYOVAzjDko-ONksRoMTc7ho",
-  authDomain: "portfolioformdata-4caae.firebaseapp.com",
-  databaseURL: "https://portfolioformdata-4caae-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "portfolioformdata-4caae",
-  storageBucket: "portfolioformdata-4caae.appspot.com",
-  messagingSenderId: "51070256017",
-  appId: "1:51070256017:web:6f0a976ae3294e627c1030",
-  measurementId: "G-X5TZQW928B"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
